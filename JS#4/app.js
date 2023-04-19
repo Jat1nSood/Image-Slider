34d6b3e539074081686ff621327217be6659127a
Jat1nSood <117588226+Jat1nSood@users.noreply.github.com>
2023-04-19 15:31:35 +0530

    Add files via upload

---
+++ JS#4/app.js
@@ -0,0 +1,49 @@
+(function(){
+
+    const pictures  =[
+        "1",
+        "2",
+        "3",
+        "4",
+        "5"
+    ];
+
+
+    const buttons = document.querySelectorAll('.btn');
+    const imageDiv = document.querySelector('.img-container');
+
+    let counter = 0;
+
+    buttons.foreach = function(button){
+        console.log(button)
+
+        button.onclick = function(e){
+
+            if(e.target.classList.contains('left-btn')){
+                counter--;
+
+                if(counter<0){
+
+                    counter = pictures.length -1;
+                }
+
+            imageDiv.style.background = `url('img/${pictures[counter]}.jpg')`;
+            }
+
+            else if(e.target.classList.contains('right-btn')){
+                counter++;
+
+                if(counter>pictures.length-1){
+
+                    counter = 0;
+                }
+
+            imageDiv.style.background = `url('img/${pictures[counter]}.jpg')`;
+            }
+
+        }
+
+
+    }
+
+})()
